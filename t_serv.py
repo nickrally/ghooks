19a4a4fc06fee4a1f82c70e7b4543e4d3982fd3d
nmusaelian-rally <nmusaelian@rallydev.com>
2018-12-28 15:10:58 -0700

    added kafka client to tornado server

--- t_serv.py
+++ t_serv.py
@@ -1,17 +1,22 @@
 import os
 import json
 import re
+import yaml
+import time
 import tornado.ioloop
 import tornado.web
+from pykafka import KafkaClient
 
 
 class GithubHookHandler(tornado.web.RequestHandler):
+    def initialize(self, kafka_producer):
+        self.kafka_producer = kafka_producer
 
-    def send_response(self, data, status=200):
+    async def send_response(self, data, status=200):
         self.set_status(status)
         self.write(json.dumps(data))
 
-    def parse_commits(self, commits):
+    async def parse_commits(self, commits):
         prefixes = ['US', 'S', 'DE', 'F']
         commits_with_FIDs = []
         messages = [commit['message'] for commit in commits]
@@ -22,20 +27,40 @@ class GithubHookHandler(tornado.web.RequestHandler):
                 commits_with_FIDs.append([item[0].upper() for item in results])
         return commits_with_FIDs
 
-    def post(self):
+    async def post(self):
         post_data = self.get_argument("payload")
-        print("got a message of type %s from GitHub..." % type(post_data))
         payload = json.loads(post_data)
-        if self.parse_commits(payload['commits']):
+        if await self.parse_commits(payload['commits']):
             print("will be posting to a kafka topic")
+            self.kafka_producer.produce(post_data.encode())
         else:
             print("will NOT be posting to a kafka topic")
         print(post_data)
-        self.send_response({'msg': 'ok'}, status=200)
+        await self.send_response({'msg': 'ok'}, status=200)
+
+
+def read_config(config_path):
+    with open(config_path, 'r') as cf:
+        content = cf.read()
+        conf = yaml.load(content)
+    return conf
 
 def make_app():
+    config = read_config('configs/kafka_config.yml')
+    kafka_home = config.get('KafkaHome')
+    topic_name = config.get('TopicName')
+    hosts = config.get('Hosts', 'localhost:9092')
+    kafka_client = KafkaClient(hosts=hosts)
+    cmd = "%s/%s/bin/kafka-topics.sh" % (os.environ['HOME'], kafka_home)
+
+    kafka_topic = kafka_client.topics[b'%s' % topic_name.encode()]
+    if not kafka_topic:
+        os.system("%s --create --zookeeper localhost:2181 --replication-factor 2 --partitions 2 --topic %s" % (cmd, topic_name))
+    time.sleep(3)
+
+    kafka_producer = kafka_topic.get_producer()
     return tornado.web.Application([
-        (r"/", GithubHookHandler),
+        (r"/", GithubHookHandler, dict(kafka_producer=kafka_producer, )),
     ])
 
 if __name__ == "__main__":
