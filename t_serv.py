f6df6137c9dbac6637e6358be9fb9196cd8cc527
nmusaelian-rally <nmusaelian@rallydev.com>
2018-12-28 23:08:22 -0700

    added zookeeper host to config

--- t_serv.py
+++ t_serv.py
@@ -50,12 +50,16 @@ def make_app():
     kafka_home = config.get('KafkaHome')
     topic_name = config.get('TopicName')
     hosts = config.get('Hosts', 'localhost:9092')
+    replication_factor = config.get('Replication', 1)
+    partitions = config.get('Partitions', 1)
+    zookeeper  = config.get('Zookeeper', 'localhost:2181')
     kafka_client = KafkaClient(hosts=hosts)
     cmd = "%s/%s/bin/kafka-topics.sh" % (os.environ['HOME'], kafka_home)
 
     kafka_topic = kafka_client.topics[b'%s' % topic_name.encode()]
     if not kafka_topic:
-        os.system("%s --create --zookeeper localhost:2181 --replication-factor 2 --partitions 2 --topic %s" % (cmd, topic_name))
+        os.system("%s --create --zookeeper %s --replication-factor %s --partitions %s --topic %s"
+                  % (cmd, zookeeper, replication_factor, partitions, topic_name ))
     time.sleep(3)
 
     kafka_producer = kafka_topic.get_producer()
